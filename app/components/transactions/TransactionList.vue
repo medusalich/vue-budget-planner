cd0b0301c18af14e3b2ff234b2d0e28fc3efe697
StephanieH <stephanie@familie-hartwig.online>
2026-09-11 14:34:01 +0200

    feat: list the transactions with their date and amount

---
+++ app/components/transactions/TransactionList.vue
@@ -0,0 +1,15 @@
+<template>
+  <h2>Buchungen</h2>
+  <ul>
+    <li v-for="transaction in transactions" :key="transaction.id">
+      {{ transaction.booked_on }} |
+      {{ formatCentsAsEuro(transaction.amount_cents) }}
+    </li>
+  </ul>
+</template>
+
+<script setup lang="ts">
+  const { transactions, loadTransactions } = useTransactions();
+
+  onMounted(loadTransactions);
+</script>
