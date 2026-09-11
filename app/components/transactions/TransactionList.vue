46e6584f284c0cc9f1415d7d27539109fbc51fa0
StephanieH <stephanie@familie-hartwig.online>
2026-09-11 16:40:04 +0200

    feat: show the category of each transaction

--- app/components/transactions/TransactionList.vue
+++ app/components/transactions/TransactionList.vue
@@ -4,13 +4,16 @@
   <ul v-else>
     <li v-for="transaction in transactions" :key="transaction.id">
       {{ formatIsoDateAsGermanDate(transaction.booked_on) }} |
-      {{ formatCentsAsEuro(transaction.amount_cents) }}
+      {{ formatCentsAsEuro(transaction.amount_cents) }} |
+      {{ findCategoryById(transaction.category_id)?.name }}
     </li>
   </ul>
 </template>
 
 <script setup lang="ts">
   const { transactions, loadTransactions, isLoading } = useTransactions();
+  const { loadCategories, findCategoryById } = useCategories();
 
   onMounted(loadTransactions);
+  onMounted(loadCategories);
 </script>
